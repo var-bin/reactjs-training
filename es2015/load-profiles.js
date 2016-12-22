ca4ba9fa65a90221c1861f72671ee986c9081dc3
var-bin <var-bin@users.noreply.github.com>
2016-12-22 16:39:13 +0200

    Start working with ES2015. Try let

---
+++ es2015/load-profiles.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function loadProfiles(userNames) {
+  let message = "Loading " + userNames.length + " user(s)";
+  
+  _displayFlash(message);
+  
+  _fetchProfiles(userNames, function (data) {
+    let profiles = data.profiles;
+    
+    _addToPage(profiles);
+  });
+}
