cae71f41813215387c64dccea92ca3293bcce6ed
var-bin <var-bin@users.noreply.github.com>
2016-12-23 15:39:18 +0200

    Refactoring. Using rest parameters

--- es2015/append-user-names.js
+++ es2015/append-user-names.js
@@ -1,11 +1,11 @@
 "use strict";
 
-function appendUserNames() {
+function appendUserNames(...userNames) {
   let userNameDivs = "";
   let USER_CLASS = ".forum-user";
   
-  for (let i in arguments) {
-    let name = arguments[i];
+  for (let i in userNames) {
+    let name = userNames[i];
     
     if (name !== "admin") {
       userNameDivs += "<div class='" + USER_CLASS + "'>" + name + "</div>";
