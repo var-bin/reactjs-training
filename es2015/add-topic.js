0f55d38a4da4ebd412f30c8d0bd424881f96a153
var-bin <var-bin@users.noreply.github.com>
2016-12-26 15:00:03 +0200

    Working with objects

---
+++ es2015/add-topic.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let title  = "The New Object Syntax - Good or Bad?";
+let author = fullName;
+let body = "What do you all think of the new syntax? I like it!";
+
+let topic = { title, author, body };
+
+let element = buildTopicElement(topic);
+
+addTopicToPage(element);
