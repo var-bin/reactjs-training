f4f9e46b705a093cc737a17f8677eb970ce3835f
var-bin <var-bin@users.noreply.github.com>
2016-12-26 14:57:08 +0200

    Working with objects

---
+++ es2015/build-topic-element.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function buildTopicElement(topic){
+  let title = "<h2>" + topic.title + "</h2>";
+  let author = "<small>" + topic.author + "</small>";
+  let body = "<p>" + topic.body + "</p>";
+
+  return { title: title, author: author, body: body };
+}
