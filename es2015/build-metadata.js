f9eea237ec36d8fb6dd3e48dfd1b0a72695ae126
var-bin <var-bin@users.noreply.github.com>
2016-12-26 15:26:08 +0200

    Refactoring. Using shorthand for functions in object. Add new constant. Replace magic number

--- es2015/build-metadata.js
+++ es2015/build-metadata.js
@@ -4,15 +4,14 @@ function buildMetadata(object){
   let id = parseInt(object.id);
   let lastUpdatedAt = object.updatedAt || object.createdAt;
   let hashCode = _buildHashCode(object);
-  
-  
+  const SECURE_HASH_CODE_LENGTH = 32;
   
   return { 
     id, 
     lastUpdatedAt, 
     hashCode,
-    isSecureHash: function() {
-      return hashCode >= 32;
+    isSecureHash() {
+      return hashCode >= SECURE_HASH_CODE_LENGTH;
     }
   };
 }
