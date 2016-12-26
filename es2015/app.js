d81d9ae33b40bf933f58a8f868b3417614fb736b
var-bin <var-bin@users.noreply.github.com>
2016-12-26 17:52:05 +0200

    Destructuring from return values

--- es2015/app.js
+++ es2015/app.js
@@ -3,6 +3,8 @@
 function buildTopicInfo(topic){
   let title = `<h1>${topic.title}</h1>`;
   let author = `<small>${topic.author}<small>`;
+  
+  return [title, author];
 }
 
 let topic = getCurrentTopic();
