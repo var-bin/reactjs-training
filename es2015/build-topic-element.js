ad805fa2d2b0124df0080faae28c15ef0e226c0d
var-bin <var-bin@users.noreply.github.com>
2016-12-26 15:41:12 +0200

    Refactor the code to use string templates.

--- es2015/build-topic-element.js
+++ es2015/build-topic-element.js
@@ -1,9 +1,9 @@
 "use strict";
 
 function buildTopicElement(topic){
-  let title = "<h2>" + topic.title + "</h2>";
-  let author = "<small>" + topic.author + "</small>";
-  let body = "<p>" + topic.body + "</p>";
+let title = `<h2>${topic.title}</h2>`;
+  let author = `<small>${topic.author}</small>`;
+  let body = `<p>${topic.body}</p>`;
 
   return { title, author, body };
 }
