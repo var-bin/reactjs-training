dd5d60b8ac3ba415eb1b3ab555f5d28a84ea11fa
var-bin <var-bin@users.noreply.github.com>
2016-12-27 12:21:44 +0200

    Continue working with Map

---
+++ es2015/listing-entries.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let recentPosts = new Map();
+
+recentPosts.set("Sam", "ES2015");
+recentPosts.set("Tyler", "CoffeeScript");
+recentPosts.set("Brook", "TypeScript");
+
+for (let [user, postTitle] of recentPosts) {
+  console.log(`${user} = ${postTitle}`);
+}
