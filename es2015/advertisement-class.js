8cac795716c458337f9e55bf515e5e98c425d963
var-bin <var-bin@users.noreply.github.com>
2016-12-27 16:35:25 +0200

    Working with subclassing

---
+++ es2015/advertisement-class.js
@@ -0,0 +1,28 @@
+"use strict";
+
+class Advertisement {
+
+  constructor(title, link){
+    this.title = title;
+    this.link = link;
+  }
+
+  _buildContent(){
+    return `<h1>${this.title}</h1>
+      <a href="${this.link}">${this._linkText()}</a>`;
+  }
+  
+  _linkText(){
+    return "Click Here";
+  }
+  
+  render(){
+    return this._buildContent();
+  }
+}
+
+// Here's an example of how this class is used:
+/*
+  let ad = new Advertisement("CodeSchool", "http://codeschool.com");
+  _appendToPage( targetElement, ad.render() );
+*/
