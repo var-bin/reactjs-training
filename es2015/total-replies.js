0732375524b06ea6fc80d74c5c5dad58d272baf2
var-bin <var-bin@users.noreply.github.com>
2016-12-27 12:13:42 +0200

    Using Map and methods of Map

--- es2015/total-replies.js
+++ es2015/total-replies.js
@@ -3,7 +3,9 @@
 let author1 = { name: "Sam" };
 let author2 = { name: "Tyler" };
 
-let totalReplies = ;
+let totalReplies = new Map();
+totalReplies.set(author1, 42);
+totalReplies.set(author2, 15);
 
 console.log( `Total Replies: ${totalReplies.get(author1)}` );
 console.log( `Total Replies: ${totalReplies.get(author2)}` );
