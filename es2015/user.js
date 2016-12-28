bb9154c0f6b227a886c261bd8136d645d9778c13
var-bin <var-bin@users.noreply.github.com>
2016-12-28 16:24:18 +0200

    Start working with iterators

---
+++ es2015/user.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let user = {
+  name: "Sam",
+  totalReplies: 17,
+  isBlocked: false
+};
+
+user[Symbol.iterator] = function () {
+  let properties = Object.keys(this);
+  let count = 0;
+  let isDone = false;
+  
+  let next = () => {
+    if (count >= properties.lenght) {
+      isDone = true;
+    }
+    
+    let value = this[properties[count++]];
+    
+    return;
+  };
+  
+  return { next };
+};
