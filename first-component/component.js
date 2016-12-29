5aeae3e09b2b1f0020b2116df6167a32a0d70845
var-bin <var-bin@users.noreply.github.com>
2016-12-29 13:29:44 +0200

    Understanding JSX

--- first-component/component.js
+++ first-component/component.js
@@ -2,7 +2,24 @@
 
 class StoryBox extends React.Component {
   render() {
-    return( <div>Story Box</div> );
+    const now = new Date();
+    const topicLists = ["HTML", "JavaScript", "React"];
+    
+    return(
+      <section>
+        <h3>
+          Story Box
+        </h3>
+        <p className="lead">
+          Current time: {now.toTimeString()}
+        </p>
+        <ul>
+          {topicLists.map(
+            topic => <li className={topic}>{topic}</li>
+          )}
+        </ul>
+      </section>
+     );
   }
 }
 
