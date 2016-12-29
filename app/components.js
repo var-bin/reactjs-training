b699913a9256409211475b4ef8b26fd6b20a7a6e
var-bin <var-bin@users.noreply.github.com>
2016-12-29 18:55:36 +0200

    Refactoring. Replace hardcode text to dynamic props. Add  _getCommentsTitle, _getComments methods

--- app/components.js
+++ app/components.js
@@ -3,18 +3,53 @@
 // components.js
 
 class CommentBox extends React.Component {
+  _getComments() {
+    const commentList = [
+      {
+        id: 1,
+        author: "Morgan McCircuit",
+        body: "Great picture!"
+      },
+      {
+        id: 2,
+        author: "Bending Bender",
+        body: "Excellent style!"
+      },
+      {
+        id: 3,
+        author: "Bending Bender 1",
+        body: "Excellent style!!!"
+      }
+    ];
+
+    return commentList.map( (comment) => {
+      return (<Comment author={comment.author} body={comment.body} key={comment.id}/>)
+    })
+  };
+
+  _getCommentsTitle(commentsCount) {
+    if (commentsCount === 0) {
+      return "No comments yet";
+    } else if (commentsCount === 1) {
+      return "1 comment"
+    }
+
+    return `${commentsCount} comments`;
+  };
+
   render() {
+    const comments = this._getComments();
+
     return(
       <div className="comment-box">
         <h3>
           Comments
         </h3>
         <h4 className="comment-count">
-          2 comments
+          {this._getCommentsTitle(comments.length)}
         </h4>
         <div className="comment-list">
-          <Comment author="Morgan McCircuit" body="Great picture!" />
-          <Comment author="Bending Bender" body="Excellent style!" />
+          {comments}
         </div>
       </div>
     );
