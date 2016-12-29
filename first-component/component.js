89917affcaaf692288ec2075b82734a2eeda70e4
var-bin <var-bin@users.noreply.github.com>
2016-12-29 12:05:56 +0200

    First component with React.js

---
+++ first-component/component.js
@@ -0,0 +1,11 @@
+"use strict";
+
+class StoryBox extends React.Component {
+  render() {
+    return( <div>Story Box</div> );
+  }
+}
+
+ReactDOM.render(
+  <StoryBox />, document.getElementById('story-app')
+);
