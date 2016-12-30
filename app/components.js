85fe36ff45415c8409318bd66d9f77f028c67878
var-bin <var-bin@users.noreply.github.com>
2016-12-30 13:47:34 +0200

    Add avatarUrl to each object in commentList. Add _toggleAbuse, _getPopularMessage methods. Edit _getCommentsTitle. Working with components states

--- app/components.js
+++ app/components.js
@@ -8,26 +8,33 @@ class CommentBox extends React.Component {
       {
         id: 1,
         author: "Morgan McCircuit",
-        body: "Great picture!"
+        body: "Great picture!",
+        avatarUrl: './assets/images/default-avatar.png'
       },
       {
         id: 2,
         author: "Bending Bender",
-        body: "Excellent style!"
+        body: "Excellent style!",
+        avatarUrl: './assets/images/default-avatar.png'
       },
       {
         id: 3,
         author: "Bending Bender 1",
-        body: "Excellent style!!!"
+        body: "Excellent style!!!",
+        avatarUrl: './assets/images/default-avatar.png'
       }
     ];
 
     return commentList.map( (comment) => {
-      return (<Comment author={comment.author} body={comment.body} key={comment.id}/>)
+      return (<Comment
+        author={comment.author}
+        body={comment.body}
+        avatarUrl={comment.avatarUrl}
+        key={comment.id}/>)
     })
   };
 
-  _getCommentsTitle(commentsCount) {
+  _getCommentsTitle(commentsCount = 0) {
     if (commentsCount === 0) {
       return "No comments yet";
     } else if (commentsCount === 1) {
@@ -35,6 +42,18 @@ class CommentBox extends React.Component {
     }
 
     return `${commentsCount} comments`;
+  };
+
+  _getPopularMessage(commentsCount = 0) {
+    const POPULAR_COUNT = 10;
+
+    if (commentsCount > POPULAR_COUNT) {
+      return (
+        <div>
+          This post is getting really popular, don't miss out!
+        </div>
+      )
+    }
   };
 
   render() {
@@ -45,6 +64,7 @@ class CommentBox extends React.Component {
         <h3>
           Comments
         </h3>
+        {this._getPopularMessage(comments.length)}
         <h4 className="comment-count">
           {this._getCommentsTitle(comments.length)}
         </h4>
@@ -57,18 +77,53 @@ class CommentBox extends React.Component {
 }
 
 class Comment extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isAbusive: false
+    };
+  }
+
+  _toggleAbuse(event) {
+    event.preventDefault();
+
+    if (!this.state.isAbusive) {
+      return this.setState({
+        isAbusive: true
+      });
+    }
+
+    return this.setState({
+      isAbusive: false
+    });
+  }
+
   render() {
+
+    let commentBody;
+
+    if (!this.state.isAbusive) {
+      commentBody = this.props.body;
+    } else {
+      commentBody = <em>Content marked as abusive</em>;
+    }
+
     return(
       <div className="comment">
+        <img src={this.props.avatarUrl} alt={`${this.props.author}'s picture`} />
         <p className="comment-header">
           {this.props.author}
         </p>
         <p className="comment-body">
-          {this.props.body}
+          {commentBody}
         </p>
-        <div className="comment-footer">
-          <a href="#" className="comment-footer-delete">
+        <div className="comment-actions">
+          <a href="#">
             Delete comment
+          </a>
+          <a href="#" onClick={this._toggleAbuse.bind(this)}>
+            Report as Abuse
           </a>
         </div>
       </div>
