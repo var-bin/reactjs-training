5f382753c4cb9fa0c0d58667d6635eec29d6ec3b
var-bin <var-bin@users.noreply.github.com>
2016-12-30 15:05:16 +0200

    Add new class CommentForm. Start working with refs

--- app/components.js
+++ app/components.js
@@ -76,6 +76,27 @@ class CommentBox extends React.Component {
   }
 }
 
+class CommentForm extends React.Component {
+  render() {
+    return (
+      <form className="comment-form" onSubmit={this._handleSubmit.bind(this)}>
+        <label>
+          New comment
+        </label>
+        <div className="comment-form-fields">
+          <input placeholder="Name:"/>
+          <textarea placeholder="Comment:"></textarea>
+        </div>
+        <div className="comment-form-actions">
+          <button type="submit">
+            Post comment
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
 class Comment extends React.Component {
   constructor() {
     super();
