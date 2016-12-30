d62a9ee15372eeb7771ace59d310bdfc71b4ced7
var-bin <var-bin@users.noreply.github.com>
2016-12-30 16:06:04 +0200

    Edit CommentBox, CommentForm. Replace comments array to state in constructor. Works with refs, props, events

--- app/components.js
+++ app/components.js
@@ -3,34 +3,41 @@
 // components.js
 
 class CommentBox extends React.Component {
-  _getComments() {
-    const commentList = [
-      {
-        id: 1,
-        author: "Morgan McCircuit",
-        body: "Great picture!",
-        avatarUrl: './assets/images/default-avatar.png'
-      },
-      {
-        id: 2,
-        author: "Bending Bender",
-        body: "Excellent style!",
-        avatarUrl: './assets/images/default-avatar.png'
-      },
-      {
-        id: 3,
-        author: "Bending Bender 1",
-        body: "Excellent style!!!",
-        avatarUrl: './assets/images/default-avatar.png'
-      }
-    ];
+  constructor() {
+    super();
 
-    return commentList.map( (comment) => {
+    this.state = {
+      showComments: false,
+      comments: [
+        {
+          id: 1,
+          author: "Morgan McCircuit",
+          body: "Great picture!",
+          avatarUrl: './assets/images/default-avatar.png'
+        },
+        {
+          id: 2,
+          author: "Bending Bender",
+          body: "Excellent style!",
+          avatarUrl: './assets/images/default-avatar.png'
+        },
+        {
+          id: 3,
+          author: "Bending Bender 1",
+          body: "Excellent style!!!",
+          avatarUrl: './assets/images/default-avatar.png'
+        }
+      ]
+    }
+  }
+
+  _getComments() {
+    return this.state.comments.map( (comment) => {
       return (<Comment
         author={comment.author}
         body={comment.body}
         avatarUrl={comment.avatarUrl}
-        key={comment.id}/>)
+        key={comment.id} />)
     })
   };
 
@@ -56,11 +63,24 @@ class CommentBox extends React.Component {
     }
   };
 
+  _addComment(author, body) {
+    const comment = {
+      id: this.state.comments.length + 1,
+      author,
+      body,
+      avatarUrl: this.state.comments[0].avatarUrl
+    };
+    // concat works better than push
+    // New array references help React stay fast
+    this.setState({comments: this.state.comments.concat([comment])});
+  }
+
   render() {
     const comments = this._getComments();
 
     return(
       <div className="comment-box">
+        <CommentForm addComment={this._addComment.bind(this)} />
         <h3>
           Comments
         </h3>
@@ -77,6 +97,38 @@ class CommentBox extends React.Component {
 }
 
 class CommentForm extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      characters: 0
+    }
+  }
+
+  _handleSubmit(event) {
+    event.preventDefault();
+
+    if (!this._author.value || !this._body.value) {
+      alert("Please enter your name and comment");
+      return;
+    }
+
+    this.props.addComment(this._author.value, this._body.value);
+
+    this._author.value = "";
+    this._body.value = "";
+
+    this.setState({
+      characters: 0
+    });
+  }
+
+  _getCharacterCount() {
+    this.setState({
+      characters: this._body.value.length
+    });
+  }
+
   render() {
     return (
       <form className="comment-form" onSubmit={this._handleSubmit.bind(this)}>
@@ -84,8 +136,11 @@ class CommentForm extends React.Component {
           New comment
         </label>
         <div className="comment-form-fields">
-          <input placeholder="Name:"/>
-          <textarea placeholder="Comment:"></textarea>
+          <input placeholder="Name:" ref={(input) => this._author = input}/>
+          <textarea placeholder="Comment:" ref={(textarea) => this._body = textarea} onKeyUp={this._getCharacterCount.bind(this)}></textarea>
+          <div>
+            {`${this.state.characters} characters`}
+          </div>
         </div>
         <div className="comment-form-actions">
           <button type="submit">
