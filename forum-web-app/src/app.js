ebea1a7db0467746c63cc57dc8bc6d360dca880d
var-bin <var-bin@users.noreply.github.com>
2017-01-04 13:20:47 +0200

    Replace Post logic to post.js Using modules

--- forum-web-app/src/app.js
+++ forum-web-app/src/app.js
@@ -2,29 +2,7 @@
 
 // src/app.js
 
-let Post = {
-  findAll() {
-    return new Promise( (resolve, reject) => {
-      let uri = "http://localhost:3000/posts";
-      let request = new XMLHttpRequest();
-
-      request.open("GET", uri, true);
-      request.onload = () => {
-        let status = request.status;
-
-        if (status >= 200 && status < 400) {
-          resolve(JSON.parse(request.response));
-        }
-      };
-
-      request.onerror = () => {
-        reject(new Error("Something went wrong on the API"));
-      }
-
-      request.send();
-    });
-  }
-};
+import { Post } from "./post";
 
 let ui = {
   renderPosts(posts) {
--- forum-web-app/src/post.js
+++ forum-web-app/src/post.js
@@ -2,3 +2,28 @@
 
 // src/post.js
 
+let Post = {
+  findAll() {
+    return new Promise( (resolve, reject) => {
+      let uri = "http://localhost:3000/posts";
+      let request = new XMLHttpRequest();
+
+      request.open("GET", uri, true);
+      request.onload = () => {
+        let status = request.status;
+
+        if (status >= 200 && status < 400) {
+          resolve(JSON.parse(request.response));
+        }
+      };
+
+      request.onerror = () => {
+        reject(new Error("Something went wrong on the API"));
+      }
+
+      request.send();
+    });
+  }
+};
+
+export { Post };
