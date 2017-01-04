8bceb52e0a3571e474fead6c6b15c49f0cbb4bf5
var-bin <var-bin@users.noreply.github.com>
2017-01-04 16:16:02 +0200

    Edit spaces for Post.findAll(). Add User.findRecent()

--- forum-web-app/src/app.js
+++ forum-web-app/src/app.js
@@ -6,5 +6,13 @@ import { Post } from "./post";
 import { ui } from "./ui";
 
 Post.findAll()
-.then(ui.renderPosts)
-.catch( (error) => console.error("Error: ", error));
+  .then(ui.renderPosts)
+  .catch( (error) => {
+    console.error("Error: ", error)
+  });
+
+User.findRecent()
+  .then(ui.renderUsers)
+  .catch( (error) => {
+    console.error("Error: ", error)
+  });
