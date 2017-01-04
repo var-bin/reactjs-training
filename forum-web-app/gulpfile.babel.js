1c912c8c239e329154803d2237b75914a486817a
var-bin <var-bin@users.noreply.github.com>
2017-01-04 15:52:49 +0200

    Some refactoring. Using const and path

--- forum-web-app/gulpfile.babel.js
+++ forum-web-app/gulpfile.babel.js
@@ -1,19 +1,25 @@
 import gulp from "gulp";
+import path from "path";
 import browserify from "browserify";
 import source from "vinyl-source-stream";
 
+const DEST_PATH = path.normalize("public/dist");
+const SRC_PATH = path.normalize("src/app.js");
+const BUNDLE_NAME = "bundle.js";
+const WATCH_SRC_PATH = "src/**/*.js";
+
 gulp.task("transpile", () => {
 
-  return browserify("src/app.js")
+  return browserify(SRC_PATH)
     .transform("babelify")
     .bundle()
-    .pipe(source("bundle.js"))
-    .pipe(gulp.dest("dist"));
+    .pipe(source(BUNDLE_NAME))
+    .pipe(gulp.dest(DEST_PATH));
 
 });
 
 gulp.task("watch", ["transpile"], () => {
-  gulp.watch("src/**/*.js", ["transpile"]);
+  gulp.watch(WATCH_SRC_PATH, ["transpile"]);
 });
 
 gulp.task("default", ["transpile"]);
