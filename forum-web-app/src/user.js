1ef09a27d006eba40cf7498a19de13fc17140b0d
var-bin <var-bin@users.noreply.github.com>
2017-01-04 17:11:14 +0200

    Refactoring. Using API fetch method instead plain script

--- forum-web-app/src/user.js
+++ forum-web-app/src/user.js
@@ -2,27 +2,11 @@
 
 // src/user.js
 
+import { API } from "./api";
+
 let User = {
   findRecent() {
-    return new Promise( (resolve, reject) => {
-      let uri = "http://localhost:3000/activeUsers";
-      let request = new XMLHttpRequest();
-
-      request.open("GET", uri, true);
-      request.onload = () => {
-        let status = request.status;
-
-        if (status >= 200 && status < 400) {
-          resolve(JSON.parse(request.response));
-        }
-      };
-
-      request.onerror = () => {
-        reject(new Error("Something went wrong on the API"));
-      }
-
-      request.send();
-    });
+    return API.fetch("activeUsers");
   }
 };
 
