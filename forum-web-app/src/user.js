613567a51ffda7a26106894689e4e14ac2712a28
var-bin <var-bin@users.noreply.github.com>
2017-01-04 16:45:59 +0200

    Add user module

---
+++ forum-web-app/src/user.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// src/user.js
+
+let User = {
+  findRecent() {
+    return new Promise( (resolve, reject) => {
+      let uri = "http://localhost:3000/activeUsers";
+      let request = new XMLHttpRequest();
+
+      request.open("GET", uri, true);
+      request.onload = () => {
+        let status = request.status;
+
+        if (status >= 200 && status < 400) {
+          resolve(JSON.parse(request.response));
+        }
+      };
+
+      request.onerror = () => {
+        reject(new Error("Something went wrong on the API"));
+      }
+
+      request.send();
+    });
+  }
+};
+
+export { User };
