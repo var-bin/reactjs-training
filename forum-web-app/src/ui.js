86b3ad30b978d227bd3b596d176c8d11b2a1995d
var-bin <var-bin@users.noreply.github.com>
2017-01-04 14:22:54 +0200

    Add articleTemplate method. Edit renderPosts

--- forum-web-app/src/ui.js
+++ forum-web-app/src/ui.js
@@ -4,8 +4,29 @@
 
 let ui = {
   renderPosts(posts) {
-    console.log(posts);
+    let elements = posts.map( (post) => {
+      let { title, lastReply } = post;
+
+      return articleTemplate(title, lastReply);
+    });
+
+    let target = document.querySelector(".container");
+    target.innerHTML = elements.join("");
   }
 };
+
+function articleTemplate(title, lastReply) {
+  let template = `
+    <article class="post">
+      <h2 class="post-title">
+        ${title}
+      </h2>
+      <p class="post-meta">
+        ${lastReply}
+      </p>
+    </article>`;
+
+  return template;
+}
 
 export { ui };
