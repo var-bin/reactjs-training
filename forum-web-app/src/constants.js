2d519bffe5092a7f8eeb0f759ed6a2f2817ed456
var-bin <var-bin@users.noreply.github.com>
2017-01-04 17:33:18 +0200

    Add constants module

---
+++ forum-web-app/src/constants.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// src/constants.js
+
+const BASE_URI = "http://localhost:3000";
+const ERROR_MESSAGE = "Something went wrong on the API";
+const POSTS_URI = "posts";
+const ACTIVE_USERS_URI = "activeUsers";
+const POSTS_DOM_TARGET = ".container";
+const USERS_DOM_TARGET = ".sidebar-content";
+
+export { BASE_URI, ERROR_MESSAGE, POSTS_URI, ACTIVE_USERS_URI, POSTS_DOM_TARGET, USERS_DOM_TARGET };
