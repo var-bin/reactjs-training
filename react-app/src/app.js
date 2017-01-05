3b78ece8bf8ecf3a125aaa203397be279f51dff0
var-bin <var-bin@users.noreply.github.com>
2017-01-05 15:24:07 +0200

    Import Router, Route, LTopMenu

--- react-app/src/app.js
+++ react-app/src/app.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import jQuery from "jquery";
 import { CommentBox } from "./components/comment-box";
 
+import { Router, Route } from 'react-router';
+import { LTopMenu } from './layouts/l-top-menu';
+
 jQuery(function() {
   ReactDOM.render(
     <CommentBox />,
