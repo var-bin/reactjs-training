f8b82c491ee243e0ebb1e93151c11c933b4d19c8
var-bin <var-bin@users.noreply.github.com>
2017-01-05 16:27:43 +0200

    Add pages

---
+++ react-app/src/pages/p-picture.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// src/pages/p-picture.js
+
+import React from 'react';
+import CommentBox from '../components/comment-box';
+
+class PPicturePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="cell">
+          <article className="article article--picture">
+            <div className="article--picture-author">
+              Picture by <strong>@morganmccircuit</strong>
+            </div>
+
+            <div className="article--picture-img">
+              <img src="assets/images/picture.jpg" />
+            </div>
+          </article>
+        </div>
+
+        <CommentBox apiUrl="api/pictures/comments.json" />
+      </div>
+    )
+  }
+}
+
+export { PPicturePage };
---
+++ react-app/src/pages/p-video.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// src/pages/p-video.js
+
+import React from 'react';
+import CommentBox from '../components/comment-box';
+
+class PVideoPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="cell">
+          <article className="article article--video">
+
+            <div className="article--picture-author">
+              Video by <strong>@morganmccircuit</strong>
+            </div>
+
+            <div className="article--video-img">
+              <img src="assets/images/video.jpg" />
+            </div>
+
+          </article>
+        </div>
+
+        <CommentBox apiUrl="api/videos/comments.json" />
+      </div>
+    )
+  }
+}
+
+export { PVideoPage };
