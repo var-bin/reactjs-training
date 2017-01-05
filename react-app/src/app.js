8ee73c8bded54ce41f28f6f7471358e03fb01f38
var-bin <var-bin@users.noreply.github.com>
2017-01-05 18:27:38 +0200

    Delete unuseful imports. Add pages, hashHistory

--- react-app/src/app.js
+++ react-app/src/app.js
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import jQuery from "jquery";
-import { CommentBox } from "./components/comment-box";
 
-import { Router, Route } from 'react-router';
+import { hashHistory, Router, Route, Redirect } from 'react-router';
 import { LTopMenu } from './layouts/l-top-menu';
+import { PBlogPage } from './pages/p-blog';
+import { PPicturePage } from './pages/p-picture';
+import { PVideoPage } from './pages/p-video';
 
 const App = (
-  <Router>
-    <Route path="/" component={LTopMenu} ></Route>
+  <Router history={hashHistory}>
+    <Redirect from="/" to="/blog" />
+    <Route path="/" component={LTopMenu}>
+      <Route path="blog" component={PBlogPage} />
+      <Route path="picture" component={PPicturePage} />
+      <Route path="video" component={PVideoPage} />
+    </Route>
   </Router>
-)
+);
 
 jQuery(function() {
   ReactDOM.render(
