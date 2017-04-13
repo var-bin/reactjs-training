794723b312948f6a01574818048067f914eed774
var-bin <var-bin@users.noreply.github.com>
2017-04-13 18:53:47 +0300

    Add two variants

--- es2015/map-reduce/map.js
+++ es2015/map-reduce/map.js
@@ -2,9 +2,29 @@
 
 "use strict";
 
-let animals = ["cat", "dog", "fish"];
-let lengths = animals.map( (animal) => {
-  return animal.length;
-});
+/**
+ * map() with anonymous in-line functions
+ */
 
-console.log(lengths);
+function animals() {
+  let animals = ["cat", "dog", "fish"];
+  let lengths = animals.map( (animal) => {
+    return animal.length;
+  });
+
+  console.log(`animals(): ${lengths}`);
+}
+
+animals();
+
+function animalsV1() {
+  let animals = ["cat", "dog", "fish"];
+
+  function _getLength(word) {
+    return word.length;
+  }
+
+  console.log("animalsV1(): " + animals.map(_getLength));
+}
+
+animalsV1();
