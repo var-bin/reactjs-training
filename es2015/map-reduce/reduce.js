b4a7587cac9625841dc670bc9fd997cf0bfb0b8b
var-bin <var-bin@users.noreply.github.com>
2017-04-14 12:34:04 +0300

    Add reduce() with anonymous in-line function

--- es2015/map-reduce/reduce.js
+++ es2015/map-reduce/reduce.js
@@ -1,3 +1,21 @@
 // reduce.js
 
 "use strict";
+
+/**
+ * reduce() with anonymous in-line function
+ */
+
+function animals() {
+  let animals = ["cat", "dog", "fish"];
+
+  let total = animals.reduce( (sum, word) => {
+    console.log(`sum: ${sum}, word: ${word}`);
+
+    return sum + word.length;
+  }, 0);
+
+  console.log(`animals(): ${total} ${animals}`);
+}
+
+animals();
