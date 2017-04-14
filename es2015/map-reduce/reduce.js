14b41136b9a4172972ed97a049e933904efbf583
var-bin <var-bin@users.noreply.github.com>
2017-04-14 12:40:56 +0300

    Add reduce() with named function

--- es2015/map-reduce/reduce.js
+++ es2015/map-reduce/reduce.js
@@ -19,3 +19,22 @@ function animals() {
 }
 
 animals();
+
+/**
+ * reduce() with named function
+ */
+function animalsV1() {
+  let animals = ["cat", "dog", "fish"];
+
+  function _addLength(sum, word) {
+    console.log(`sum: ${sum}, word: ${word}`);
+
+    return sum + word.length;
+  }
+
+  let total = animals.reduce(_addLength, 0);
+
+  console.log(`animalsV1(): ${total} ${animals}`);
+}
+
+animalsV1();
